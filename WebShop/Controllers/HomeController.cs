e769922002af553210871d04eeeb0deba1bd93f6
rostyslavskarbek <rostyslav.skarbek@gmail.com>
2014-09-24 21:02:22 +0200

    Initial commit.

---
+++ WebShop/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace TryCatch.WebShop.Controllers
+{
+    public class HomeController : Controller
+    {
+        public string Index()
+        {
+            return "WebShop Start Page";
+        }
+
+    }
+}
